2cc4c2a7d86f6231129a9c84ca6f461787923d57
Karan Anand <karan94@Karans-Mac-mini.local>
2025-07-29 10:47:08 +0200

    Processing graph for one airfoil

---
+++ preprocessing/process_airfoil_dataset.py
@@ -0,0 +1,40 @@
+import os
+import pickle
+import numpy as np
+from tqdm import tqdm
+from graph_from_snapshot import build_graph_from_snapshot
+
+# Config
+DATA_DIR = "/Users/karan94/Documents/MeshGraphNets_Airfoil/data/Airfoil"
+SAVE_DIR = "/Users/karan94/Documents/MeshGraphNets_Airfoil/data/graphs_test_airfoil"
+os.makedirs(SAVE_DIR, exist_ok=True)
+
+airfoil_id = 0
+
+print("Loading raw data...")
+grid = np.load(f"{DATA_DIR}/test_grid.npy")
+cells = np.load(f"{DATA_DIR}/test_cells.npy")
+data = np.load(f"{DATA_DIR}/test_data.npy")
+
+num_timesteps = grid.shape[2]
+
+print(f"Building graphs for airfoil {airfoil_id}")
+
+for frame in tqdm(range(num_timesteps - 1)):
+    # Inputs
+    node_pos = grid[airfoil_id, :, frame, :]        # [N, 2]
+    triangles = cells[airfoil_id, :, frame, :]      # [T, 3]
+    features = data[airfoil_id, :, frame, :3]       # [N, 3] u, v, p
+    target = data[airfoil_id, :, frame + 1, :3]     # [N,3] next timestep
+    node_type = np.zeros((features.shape[0],))
+
+    # Build graph
+    graph = build_graph_from_snapshot(node_pos, triangles, features, target, node_type)
+
+    # Save to *.pkl files
+    fname = os.path.join(SAVE_DIR, f"graph_{frame:05d}.pkl")
+    with open(fname, "wb") as f:
+        pickle.dump(graph, f)
+
+print(f"Saved {num_timesteps - 1} graphs to {SAVE_DIR}")
+
